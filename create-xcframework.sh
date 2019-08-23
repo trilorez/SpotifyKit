f0fdc1694cce37c39452294f7e34c56529b42015
Daniel Resnick <danielzresnick@gmail.com>
2019-08-22 19:59:55 -0700

    Make sure to delete existing xcframework before making a new one

--- create-xcframework.sh
+++ create-xcframework.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+OUTPUT=build/SpotifyKit.xcframework
 IOS_ARCHIVE=build/ios/SpotifyKit.xcarchive
 IOS_SIMULATOR_ARCHIVE=build/ios-simulator/SpotifyKit.xcarchive
+
+rm -rf $OUTPUT
+
 xcodebuild archive -scheme SpotifyKit -destination="iOS" -sdk iphoneos SKIP_INSTALL=NO -archivePath $IOS_ARCHIVE
 xcodebuild archive -scheme SpotifyKit -destination="iOS Simulator" -sdk iphonesimulator SKIP_INSTALL=NO -archivePath $IOS_SIMULATOR_ARCHIVE
-xcodebuild -create-xcframework -framework $IOS_ARCHIVE/Products/Library/Frameworks/SpotifyKit.framework -framework $IOS_SIMULATOR_ARCHIVE/Products/Library/Frameworks/SpotifyKit.framework -output build/SpotifyKit.xcframework
+xcodebuild -create-xcframework -framework $IOS_ARCHIVE/Products/Library/Frameworks/SpotifyKit.framework -framework $IOS_SIMULATOR_ARCHIVE/Products/Library/Frameworks/SpotifyKit.framework -output $OUTPUT
