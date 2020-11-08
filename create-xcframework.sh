70e9cbfc7cd6206425c7d0ba23da24715a9c3f0c
Daniel Resnick <danielzresnick@gmail.com>
2020-11-08 13:10:31 -0800

    Exclude arm64 from simulator archive

--- create-xcframework.sh
+++ create-xcframework.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+set -e
+
 OUTPUT=build/SpotifyKit.xcframework
 IOS_ARCHIVE=build/ios/SpotifyKit.xcarchive
 IOS_SIMULATOR_ARCHIVE=build/ios-simulator/SpotifyKit.xcarchive
 
 rm -rf $OUTPUT
 
+echo "Building for iOS"
 xcodebuild archive -scheme SpotifyKit -destination="iOS" -sdk iphoneos SKIP_INSTALL=NO -archivePath $IOS_ARCHIVE
+
+echo "Building for iOS Simulator"
 xcodebuild archive -scheme SpotifyKit -destination="iOS Simulator" -sdk iphonesimulator SKIP_INSTALL=NO -archivePath $IOS_SIMULATOR_ARCHIVE
+
+echo "Creating xcframework"
 xcodebuild -create-xcframework -framework $IOS_ARCHIVE/Products/Library/Frameworks/SpotifyKit.framework -framework $IOS_SIMULATOR_ARCHIVE/Products/Library/Frameworks/SpotifyKit.framework -output $OUTPUT
